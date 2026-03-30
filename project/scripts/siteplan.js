a3e8053fa771f4fc60f0ef7ad3b5503a40e872e5
XaidieCampbell <xcampbell@>
2026-03-30 11:36:53 -0400

    project try 1

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,6 @@
+
+
+const year = new Date().getFullYear();
+document.getElementById("currentYear").textContent = year;
+
+document.getElementById("lastModified").textContent = document.lastModified;
